6a1c3af1068b300cfcb2f1059de36c4bd3bdcccb
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2023-01-18 21:26:33 -0600

    Modific: Cors option

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+app.use(cors(corsOptions));
+// app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
