28d99ec46962356ea243e2db8d40969b233f1fdc
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2023-01-18 22:34:04 -0600

    ADD: bcrypt

--- middlewares/auth.handler.js
+++ middlewares/auth.handler.js
@@ -1,8 +1,9 @@
 const boom = require('boom');
+const {config} = require('../bin/config.js')
 
 function checkApiKey(req,res,next){
     const apiKey = req.headers['api'];
-    if(apiKey === '123'){
+    if(apiKey === config.apiKey){
         next();
     }else{
         next(boom.unauthorized());
