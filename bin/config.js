d8c8d6fa06f1c9f6b8552e96d228fb5a54fef1c5
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2023-01-18 22:21:39 -0600

    Modifci:archivo Env Add: file config

---
+++ bin/config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+
+const config = {
+    port:process.env.PORT || 5000,
+    db_user:process.env.DB_USER,
+    db_password:process.env.DB_PASSWORD,
+    db_name:process.env.DB_DBNAME,
+    apiKey:process.env.API_KEY,
+}
+
+module.exports = {config};
--- bin/mongodb.js
+++ bin/mongodb.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
-
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
-console.log(process.env.DB_DBNAME);
+const {config} = require('./config');
 
     mongoose.set('strictQuery', false);
-    const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@buyshare.t3ffzxr.mongodb.net/${process.env.DB_DBNAME}?retryWrites=true&w=majority`;
+    const uri =`mongodb+srv://${config.db_user}:${config.db_password}@buyshare.t3ffzxr.mongodb.net/${config.db_name}?retryWrites=true&w=majority`;
     mongoose.connect(uri, 
         {
             connectTimeoutMS: 1000,
