f906fcf246c01ea36b565b685c4d6c932ac9633b
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2023-01-24 00:13:11 -0600

    Add:avatares de usuarios agregados a una lista

--- services/list.service.js
+++ services/list.service.js
@@ -1,6 +1,8 @@
 const ListModel = require('../Models/List.model')
 const TaskService = require('./task.service');
+const UserService = require('./users.service');
 const taskService = new TaskService();
+const userService = new UserService();
 
 class ListService {
     constructor() {}
@@ -22,11 +24,28 @@ class ListService {
         const query = ListModel.find({ 'id_user': idUser });
         let lists = await query.exec();
         let newLists = [];
+        let newListsAvatar = [];
         for (const [index, element] of lists.entries()) {
+            //cantidad de tareas en la lista
             const cant = await taskService.getCantByIdList(element.id);
             const course = {...element._doc,'cant':cant};
             newLists.push(course);
+
+            //buscar los avatares de los usuarios de la lista
+            let listaId = [];
+            const avatar = await userService.findAvatarById(element.id_user);
+            listaId.push(avatar);
+
+            const listaUsuariosByIdList = await ListModel.find({'referencia':element.id}).exec();
+            for (const [index, usuario] of listaUsuariosByIdList.entries()) {
+                const avatar = await userService.findAvatarById(usuario.id_user);
+                listaId.push(avatar);
+            }
+            newListsAvatar.push(listaId);
+            // console.log('---------------------------------------');
         }
+        // console.log(newListsAvatar);Add:
+        newLists.push(newListsAvatar);
         return newLists;
     }
 }
