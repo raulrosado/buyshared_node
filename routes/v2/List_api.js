a0ae170775c09c49b78c4004c34bd876aca087f4
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2023-04-30 22:25:04 -0500

    Add: Insert new List server and DB

--- routes/v2/List_api.js
+++ routes/v2/List_api.js
@@ -7,6 +7,22 @@ const ListService = require("../../services/v2/list.service");
 const service = new ListService();
 const { config } = require("../../bin/config");
 
+router.post(
+  "/addList",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const listParams = {
+      id_user: req.user.sub,
+      id_event: req.body.event,
+      nombre: req.body.nombre,
+      estado: 1,
+      referencia: "",
+    };
+    const list = await service.AddList(listParams)
+    const infoList = await service.getDetailList(list._id)
+    res.json(infoList);
+  }
+);
 
 router.get(
   "/user/:id_user",
--- services/v2/list.service.js
+++ services/v2/list.service.js
@@ -7,6 +7,11 @@ const userService = new UserService();
 class ListService {
     constructor() {}
 
+    async AddList(list){
+        const newListCreate = new ListModel(list)
+        return await newListCreate.save()
+    }
+
     async findByIdUser(idUser){
         const query = ListModel.find({ 'id_user': idUser });
         let lists = await query.exec();
@@ -88,6 +93,48 @@ class ListService {
             avatarList,
             tasks,
             'cant':cant
+        };   
+        return respuesta;
+    }
+
+    async getDetailList(idList){
+        let lists = await ListModel.findOne({ '_id': idList }).exec();
+        let avatarList = [];
+        let tasks = [];
+        let cant = 0
+
+        tasks = await taskService.findByIdList(idList);
+        cant = await taskService.getCantByIdList(idList)
+        let otrasListas = []
+        const avatarU = await userService.findAvatarById(lists.id_user);
+        avatarList.push(avatarU);
+
+        if(lists.referencia !== ''){
+            otrasListas = await ListModel.find({'_id':String(lists.referencia)}).exec();
+        }else{
+            otrasListas = await ListModel.find({'referencia':String(lists._id)}).exec();
+        }
+
+        for (const [index, user] of otrasListas.entries()) {
+            const avatar = await userService.findAvatarById(user.id_user);
+            avatarList.push(avatar);
+        }
+
+        for (const [index, list] of otrasListas.entries()) {
+            cant += await taskService.getCantByIdReference(list.referencia)
+            let taskR = await taskService.findByIdList(list._id);
+            for (const [index, taskRef] of taskR.entries()) {
+                tasks.push(taskRef)
+            }
+        }
+
+        const taskList = [];
+        const respuesta = {
+            ...lists._doc,
+            avatarList,
+            tasks,
+            'cant':cant,
+            'success':true,
         };   
         return respuesta;
     }
